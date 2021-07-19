64d8616dd767a4fef2e78a1e67a92e2566f44f6f
Rachel Land <rachelland@Rachels-MacBook-Pro-2.local>
2021-07-19 13:18:31 -0400

    adding files

---
+++ plots.js
@@ -0,0 +1,48 @@
+d3.json("samples.json").then(function(data){
+    firstPerson = data.metadata[0];
+    Object.entries(firstPerson).forEach(([key, value]) =>
+      {console.log(key + ': ' + value);});
+});
+
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+  }
+
+  function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+      var samples = data.samples;
+      var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+    });
+  }
